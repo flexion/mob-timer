1691754aef92ce497ab804f20758040ca26bef0c
Cody Seibert <cseibert@flexion.us>
2021-09-21 10:00:51 -0400

    using ts enum instead of boolean for state

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,16 +3,23 @@
 </script>
 
 <script lang="ts">
+
+	enum TIMER_STATE {
+		NEW,
+		RUNNING,
+		PAUSED
+	}
+
 	const TOTAL_TIME: number = 5;
 	let timeRemaining: number = 0;
 	let timer: NodeJS.Timer = null;
-	let isRunning = false;
+	let timerState: TIMER_STATE = TIMER_STATE.NEW;
 
 	function createTimer() {
 		timer = setInterval(() => {
 			timeRemaining -= 1;
 			if (timeRemaining == 0) {
-				isRunning = false;
+				timerState = TIMER_STATE.NEW;
 				clearInterval(timer);
 				setTimeout(() => alert('time is up, rotate drivers!'));
 			}
@@ -20,19 +27,19 @@
 	}
 
 	function start() {
-		isRunning = true;
+		timerState = TIMER_STATE.RUNNING;
 		timeRemaining = TOTAL_TIME;
 		createTimer();
 	}
 
 	function pause() {
-		isRunning = false;
+		timerState = TIMER_STATE.PAUSED;
 		clearInterval(timer);
 	}
 
 	function resume() {
 		createTimer();
-		isRunning = true;
+		timerState = TIMER_STATE.RUNNING;
 	}
 </script>
 
@@ -44,18 +51,20 @@
 	<h1>
 		Mob Timer
 	</h1>
+
 	<h2>
 		{timeRemaining}
 	</h2>
-	{#if timeRemaining === 0}
+
+	{#if timerState === TIMER_STATE.NEW}
 		<button on:click={start}>start</button>
 	{/if}
 
-	{#if timeRemaining > 0 && !isRunning}
+	{#if timerState === TIMER_STATE.PAUSED}
 		<button on:click={resume}>resume</button>
 	{/if}
 
-	{#if isRunning}
+	{#if timerState === TIMER_STATE.RUNNING}
 		<button on:click={pause}>pause</button>
 	{/if}
 </section>
